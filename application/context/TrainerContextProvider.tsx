3fc997ab881e4154644f8d674f5605dbb3b54841
C-Fruit-Stu <124798605+C-Fruit-Stu@users.noreply.github.com>
2024-10-13 19:17:11 +0300

    d

--- application/context/TrainerContextProvider.tsx
+++ application/context/TrainerContextProvider.tsx
@@ -49,7 +49,7 @@ export default function TrainerContextProvider({ children }: any) {
     
     async function GetAllTrainers() {
         try {
-            let data = await POST('trainer/getalltrainer',[]);  // No ID required here
+            let data = await GET('trainer/getalltrainer');  // No ID required here
             console.log("Fetched trainers:", data);
 
             if (data && data.trainers) {
